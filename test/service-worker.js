263e56a4c2653b24ca7af27ee581ef466cbf548a
pwatestuser <30769787+pwatestuser@users.noreply.github.com>
2017-08-06 23:35:12 +0900

    test

--- test/service-worker.js
+++ test/service-worker.js
@@ -1,16 +1,13 @@
-{
-    "name": "pwatest",
-    "short_name": "pwatest",
-    "icons": [
-        {
-            "src": "images/icons/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
-        }
-    ],
-    "start_url": "https://pwatestuser.github.io/pwatest20170806/test/",
-    "display": "standalone",
-    "theme_color": "#e91e63",
-    "gcm_sender_id": "pwatest-176008",
-    "gcm_user_visible_only": true
-}
+self.addEventListener("push", function(event){
+  event.waitUntil().then(function(res){
+    self.registration.showNotification("プッシュタイトル", {
+      body: "プッシュ通知",
+      icon: "/images/icons/icon-144x144.png",
+      tag: "tag"
+    });
+  });
+});
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  clients.openWindow("/");
+}, false);
