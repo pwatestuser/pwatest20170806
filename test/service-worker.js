b5c516bfde36cf6fb68f072bbd40dc23140cbb9e
pwatestuser <30769787+pwatestuser@users.noreply.github.com>
2017-08-06 22:58:43 +0900

    test

---
+++ test/service-worker.js
@@ -0,0 +1,16 @@
+{
+    "name": "pwatest",
+    "short_name": "pwatest",
+    "icons": [
+        {
+            "src": "images/icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+        }
+    ],
+    "start_url": "https://pwatestuser.github.io/pwatest20170806/test/",
+    "display": "standalone",
+    "theme_color": "#e91e63",
+    "gcm_sender_id": "pwatest-176008",
+    "gcm_user_visible_only": true
+}
